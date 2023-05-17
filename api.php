27a39ffda89ab1fcaf9f2b7d141961d33fedf610
OmairaBarrera <lomaira.barrera@gmail.com>
2023-05-17 08:12:26 -0500

    feat: :sparkles: php class
    config the functions initial

--- api.php
+++ api.php
@@ -1,2 +1,13 @@
 <?php
-?>
+    class olla{
+        public $lugares;
+        private $merca;
+        public function __construct(){
+
+        }
+        public function __destruct(){
+            echo "se retiro la ".__CLASS__;
+        }
+    }
+    $obj = new olla();
+?>  
